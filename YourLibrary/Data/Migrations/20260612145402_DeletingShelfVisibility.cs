60ca1bae8fcc0b8b7f9e1df12cba8852eafd521e
Fyjonn <wikikowalska333@gmail.com>
2026-06-12 16:56:02 +0200

    deleting ShelfVisibility

---
+++ YourLibrary/Data/Migrations/20260612145402_DeletingShelfVisibility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YourLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeletingShelfVisibility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShelfVisibility",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShelfVisibility",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- YourLibrary/Models/ApplicationUser.cs
+++ YourLibrary/Models/ApplicationUser.cs
@@ -10,7 +10,6 @@ namespace YourLibrary.Models
         public string Avatar { get; set; } = "🌿";
 
         public string? AvatarImagePath { get; set; }
-        public bool ShelfVisibility { get; set; } = true;
         // Klucze obce
 
         // z userbook
